257f8dc914306e159ff1ce1ddcef89c5cb1befa8
Mauricio Ilarrescape <ilarrescapem@gmail.com>
2024-10-08 22:32:56 -0300

    8-10-2024 -m app.py -m clientes.py

--- App.py
+++ App.py
@@ -1,50 +1,34 @@
 import streamlit as st
-import mysql.connector
-from mysql.connector import Error
+import pandas as pd
+from clientes import Cliente, DataManagerCliente
 
-# Función para conectarse a la base de datos
-def create_connection():
-    try:
-        connection = mysql.connector.connect(
-            host='localhost',  
-            database='Siffrah',  
-            user='root',  
-            password='root'  
-        )
-        if connection.is_connected():
-            return connection
-    except Error as e:
-        st.error(f"¡Error al conectarse a la base de datos: {e}!")
-        return None
+def main():
+    if "page" not in st.session_state:
+        st.session_state.page = "index"
 
-# Función para verificar las credenciales
-def verificar_credenciales(username, password):
-    connection = create_connection()
-    if connection:
-        cursor = connection.cursor(dictionary=True)
-        query = "SELECT * FROM usuario WHERE nombre_usuario = %s AND pass_usuario = %s"
-        cursor.execute(query, (username, password))
-        result = cursor.fetchone()
-        cursor.close()
-        connection.close()
-        return result is not None
-    return False
+    st.sidebar.title("Menu de opciones")
+    if st.sidebar.button("inicio"):
+        st.session_state.page = 'index'
+    if st.sidebar.button("Iniciar sesión"):
+        st.session_state.page = 'inicio_sesion'
+    if st.sidebar.button("Registrar usuario"):
+        st.session_state.page = 'registro_usuario'
+    if st.sidebar.button("Clientes"):
+        st.session_state.page = 'Clientes'
 
-    # Interfaz de Streamlit
-    def main():
-        st.title("Inicio de sesión")
 
-        username = st.text_input("Nombre de usuario")
-        password = st.text_input("Contraseña", type="password")
 
-        if st.button("Iniciar sesión"):
-            if username and password:
-                if verificar_credenciales(username, password):
-                    st.success(f"Bienvenido, {username}!")
-                else:
-                    st.error("Usuario o contraseña incorrectos")
-            else:
-                st.warning("Por favor, complete ambos campos")
+    if st.session_state.page =="index":
+        st.title("Siffrah")
+        st.header('Sistema de control de stock')
 
-    if __name__ == "__main__":
-        main()
+    # if st.session_state.page == 'registro_usuario':
+    #     usuario.displayRegistro()
+    
+    # if st.session_state.page == 'inicio_sesion':
+    #     usuario.displayInicioSesion()
+    if st.session_state.page == "Clientes":
+        objeto_cliente = DataManagerCliente()
+        objeto_cliente.displayClientes()        
+if __name__ == "__main__":
+    main()
--- clientes.py
+++ clientes.py
@@ -1,69 +1,110 @@
-import streamlit as st 
 import mysql.connector
-from mysql.connector import Error
-import pandas as pd 
+import streamlit as st
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import time
 
-def create_connection():
-    try:
-        connection = mysql.connector.connect(
-            host='localhost',  
-            database='Siffrah',  
-            user='root',  
-            password='root'  
+load_dotenv()
+
+class Cliente:
+    def __init__(self):
+        self.connection = mysql.connector.connect(
+            host = os.getenv('DB_HOST'),
+            user = os.getenv('DB_USER'),
+            password = os.getenv('DB_PASSWORD'),
+            database = os.getenv('DB_NAME')
         )
-        if connection.is_connected():
-            return connection
-    except Error as e:
-        st.error(f"Error al conectarse a la base de datos: {e}")
-        return None
+        self.cursor = self.connection.cursor(dictionary=True)
 
-# Interfaz de clientes
-def main():
-    connection = create_connection()
-    if connection is None: 
-        return 
-    #Funcion para agregar cliente
-    mycursor = connection.cursor()
-    op_cliente = st.sidebar.selectbox('Selecciona una opcion',("Agregar Cliente", "Buscar Cliente", "Actulizar Cliente", "Eliminar Cliente"))
-    if op_cliente == 'Agregar Cliente':
-        st.subheader('Agregar Cliente')
-        name_add = st.text_input('Introduzca el nombre del cliente: ')
-        surname = st.text_input('Introduzca el apellido del cliente: ')
-        if st.button('Agregar'):
-            cursor = "insert into clientes (nombre_cliente, apellido_cliente) values (%s,%s)"
-            val = (name_add,surname)
-            mycursor.execute(cursor,val)
-            connection.commit()
-            st.success('Cliente agregado satisfactoriamente!')                
-#Funcion para buscar y filtrar cliente
+    def obtener_dato(self):
+        self.cursor.execute('SELECT * FROM clientes')
+        result = self.cursor.fetchall()
+        return result
+    
+    def crear_cliente(self,name):
+        self.cursor.execute('SELECT nombre_cliente FROM clientes')
+        list_clientes = self.cursor.fetchall()
+        
+        if (len(list_clientes)) != 0:
+            for i in range(len(list_clientes)):
+                existente = False
+                if list_clientes[i]["nombre_cliente"] == name:
+                    st.warning(f"El cliente {name} ya está registrado en la base de datos")
+                    existente = True
+                    break
+                
+            if existente == False:
+                self.cursor.execute('SELECT * FROM clientes ORDER BY idclientes DESC LIMIT 1')
+                list_clientes = self.cursor.fetchall()
+                id = list_clientes[0]['idclientes']
+                id +=1
+                self.cursor.execute('INSERT INTO clientes(idclientes , nombre_cliente) VALUES(%s,%s)',(id,name))
+                self.connection.commit()
+                st.success("el cliente se creo correctamente")   
+        else:
+            id=1
+            self.cursor.execute('INSERT INTO clientes(idclientes , nombre_cliente) VALUES(%s,%s)',(id,name))
+            self.connection.commit()
+            st.success("el cliente se creo correctamente")
+    
+    def actualizarCliente(self,id,name):
+        self.cursor.execute('UPDATE clientes SET nombre_cliente = %s WHERE idclientes = %s',(name, id))
+        self.connection.commit()
+        st.write(name)
+        time.sleep(0.5)
+        
+    def eliminarCliente(self, id):
+        self.cursor.execute('DELETE FROM clientes WHERE idclientes = %s',(id,))
+        self.connection.commit()
+        time.sleep(0.5)
+    
+class DataManagerCliente:
+    def __init__(self) -> None:
+        self.db_cliente = Cliente()
+                
+    @st.dialog("Cargar nuevo cliente")
+    def display_crearCliente(self):
+        name = st.text_input("ingrese nombre del cliente")
+        if st.button("cargar"):
+            self.db_cliente.crear_cliente(name)
+            time.sleep(0.5)
+            st.rerun()
+            
+    @st.dialog("Modificar/eliminar cliente")
+    def modificar_cliente(self,id,name):
+        nombre_cliente = st.text_input('Ingrese el cliente', value=name)
+        col_a,col_b, col_c =  st.columns([3,2,2])
+        with col_b:
+            if st.button('Modificar', use_container_width=True):
+                self.db_cliente.actualizarCliente(id, nombre_cliente)
+        with col_c:    
+            if st.button('Eliminar', use_container_width=True):
+                self.db_cliente.eliminarCliente(id)
+                with col_a:
+                    st.success("el cliente se elimino correctamente")
+                    st.rerun()
 
-    elif op_cliente == 'Buscar Cliente':
-        st.subheader ('Buscar Cliente')
-        df = pd.read_csv('clientes.csv')
-        st.dataframe(df)
-#Funcion para actualizar/editar cliente
 
-    elif op_cliente == 'Actulizar Cliente':
-        st.subheader ('Actualizar Cliente')
-        id_reload = st.number_input('Numero de ID del cliente a actualizar: ',step=1)
-        name_reload = st.text_input('Introduzca nuevo nombre: ')
-        surname_reload = st.text_input('Introduzca nuevo apellido: ')
-        if st.button('Actualizar'):
-            sql="update clientes set nombre_cliente=%s, apellido_cliente=%s where idclientes=%s"
-            val = (name_reload, surname_reload, id_reload)
-            mycursor.execute(sql,val)
-            connection.commit()
-            st.warning('Se actualizo el cliente satisfactoriamente!')
-#Funcion para eliminar cliente
+    def displayClientes(self):
+        st.title("Clientes")
+        data_clientes = self.db_cliente.obtener_dato()
+        df_clientes = pd.DataFrame(data_clientes)
+        df_clientes = df_clientes.rename(columns={"idclientes":"ID" , "nombre_cliente":"CLIENTE"})
+        evento = st.dataframe(df_clientes , # nos dice con que dataframe vamos a trabajar
+                            hide_index=True , # escondemos la columna de los indices
+                            use_container_width=True ,  #usamos todos el ancho del contenedor
+                            height=175 , #determinamos la altura del dataframe
+                            selection_mode="single-row", #establecemos el modo de seleccion por filas simple(solo se puede seleccionar una fila a la vez)
+                            on_select="rerun")  # cuando se selecciona algun cliente , se refresca la pagina
+        if evento['selection']['rows']:
+            filtrado = df_clientes[df_clientes.index.isin(evento.selection['rows'])]
+            valor_id = int(filtrado.iloc[0,0])
+            valor_nombre = str(filtrado.iloc[0,1])
+            self.modificar_cliente(valor_id, valor_nombre)
+                    
+        if st.button("Cargar cliente"):
+            self.display_crearCliente()
+        if st.button("Modificar cliente"):
+            st.write("ds")
 
-    elif op_cliente == 'Eliminar Cliente':
-        st.subheader ('Eliminar Cliente')
-        delete = st.number_input('Introduzca el ID del cliente a borrar: ',min_value=1)
-        if st.button('Borrar'):
-            sql="delete from clientes  where idclientes=%s"
-            val = (delete,)
-            mycursor.execute(sql,val)
-            connection.commit()
-            st.error('Se borro el cliente satisfactoriamente!')
-if __name__ == "__main__":
-    main()
