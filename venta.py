3cc712585bcec9f1fe7a622ff3552d12933b09be
LautaroG137 <lauta.garay73@gmail.com>
2024-10-22 20:19:43 -0300

    Add files via upload
    actualizado clase ventas, pero aun sin terminar

--- venta.py
+++ venta.py
@@ -21,7 +21,9 @@ class Venta:
 
 if "df_productos" not in st.session_state:
     st.session_state.df_productos = pd.DataFrame(columns=["Producto","Precio Efectivo","Precio de lista","Cantidad","Total Efectivo","Total Lista"])      
-        
+if "totales" not in st.session_state:
+    st.session_state["totales"] = pd.DataFrame([[0, 0]], columns=["TOTAL EFECTIVO", "TOTAL LISTA"])      
+     
 class DataManagerVenta:
     def __init__(self) -> None:
         self.db_venta = Venta()
@@ -30,11 +32,21 @@ class DataManagerVenta:
         if "nueva_fila" not in st.session_state:
             st.session_state.nueva_fila = None
         st.title("VENTA")
-        if st.session_state.nueva_fila !=None :
+        if st.session_state.nueva_fila !=None :  
+            # aca se carga el nuevo producto a la lista de venta
             nueva_fila = pd.DataFrame([st.session_state.nueva_fila])
             st.session_state.df_productos = pd.concat([st.session_state.df_productos, nueva_fila], ignore_index=True)
             st.session_state.nueva_fila = None
-        st.write(st.session_state.df_productos)
+            # aca sumamos los precios crear el dataframe con los totales
+            totales = nueva_fila.to_numpy()
+            totalEf = totales[0,4]
+            totalList = totales[0,5]
+            st.session_state["totales"].loc[0, "TOTAL EFECTIVO"] += totalEf
+            st.session_state["totales"].loc[0, "TOTAL LISTA"] += totalList
+        st.write(st.session_state.df_productos) #se muestra en pantalla la lista de venta
+        
+        st.write(st.session_state["totales"]) # esto muestra los totales de los productos en la lista de venta
+        
         col1,col2,col3,col4 =st.columns([1, 1, 1, 1])
         with col1:
             if st.button("Cargar producto"):
@@ -70,7 +82,9 @@ class DataManagerVenta:
         if st.button("cargar"):
             df = st.session_state.df_productos['Producto']
             df = df.tolist()
-            if producto_seleccionado['stock_producto'] < cantidad:
+            if cantidad == 0:
+                st.warning("la cantidad a vender debe ser mayor a 0")
+            elif producto_seleccionado['stock_producto'] < cantidad :
                 st.warning(f"no tiene el suficiente stock de {producto_seleccionado['nombre_producto']}")
             elif producto_seleccionado['nombre_producto'] in df :
                 st.warning(f" el producto {producto_seleccionado['nombre_producto']} ya se encuentra en la lista de venta")
@@ -84,13 +98,23 @@ class DataManagerVenta:
         indice = st.number_input("que indice desea borrar?",step=1, format="%d")
         if st.button("borrar"):
             if indice == 0:
+                totalEf=(df.iloc[0, 4])
+                totalList=(df.iloc[0, 5])
+                st.session_state["totales"].loc[0, "TOTAL EFECTIVO"] -= totalEf
+                st.session_state["totales"].loc[0, "TOTAL LISTA"] -= totalList
                 df = df.drop(indice)
                 st.session_state.df_productos = df
-                st.rerun()                
-            if indice:
+                st.rerun()     
+            if indice !=0 :
+                totalEf=(df.iloc[0, 4])
+                totalList=(df.iloc[0, 5])
+                st.session_state["totales"].loc[0, "TOTAL EFECTIVO"] -= totalEf
+                st.session_state["totales"].loc[0, "TOTAL LISTA"] -= totalList
                 df = df.drop(indice)
                 st.session_state.df_productos = df
                 st.rerun()
+            st.rerun()
+            
                 
     def vender(self,df):
         df = df.to_numpy()
